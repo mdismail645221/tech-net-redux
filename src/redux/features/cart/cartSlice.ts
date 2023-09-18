b871aee5fbcac7ebfb273da4da37ea27296dc834
Ismail Hossain <you@example.com>
2023-09-18 21:16:02 +0600

    removeOneCart implement---DONE

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { IProduct } from '@/types/globalTypes';
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
@@ -24,6 +25,19 @@ const cartSlice = createSlice({
         state.products.push({ ...action.payload, quantity: 1 });
       }
     },
+    removeOneCart: (state, action: PayloadAction<IProduct>) => {
+      const exiting = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain
+      if (exiting && exiting?.quantity! > 1) {
+        exiting.quantity = exiting.quantity! - 1;
+      } else {
+        state.products = state.products.filter(
+          (product) => product._id !== action.payload._id
+        );
+      }
+    },
     removeFromCart: (state, action: PayloadAction<IProduct>) => {
       state.products = state.products.filter(
         (product) => product._id !== action.payload._id
@@ -32,6 +46,6 @@ const cartSlice = createSlice({
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, removeOneCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
