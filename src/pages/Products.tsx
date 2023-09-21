5bc9aab2016026e8a1b303d2a935828864e46699
Ismail Hossain <mdismail645221@gmail.com>
2023-09-21 08:13:48 +0600

    RTK query implement is done

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,18 +3,24 @@ import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
 import { Switch } from '@/components/ui/switch';
 import { useToast } from '@/components/ui/use-toast';
+import { useGetProductsQuery } from '@/redux/api/apiSlice';
 import { priceStatus, priceToggle } from '@/redux/features/products/productSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { IProduct } from '@/types/globalTypes';
 import { useEffect, useState } from 'react';
 
 export default function Products() {
-  const [data, setData] = useState<IProduct[]>([]);
-  useEffect(() => {
-    fetch('./data.json')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  // const [data, setData] = useState<IProduct[]>([]);
+  // useEffect(() => {
+  //   fetch('./data.json')
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data));
+  // }, []);
+
+   const {data, isLoading} = useGetProductsQuery('products')
+   console.log(data)
+
+
   // const {products, total} = useAppSelector(state=> state.cart)
 
   // const data = products
@@ -32,11 +38,11 @@ const dispatch = useAppDispatch()
   let productsData;
 
   if (status) {
-    productsData = data.filter(
-      (item) => item.status === true && item.price < priceRange
+    productsData = data?.filter(
+      (item: { status: boolean; price: number; }) => item.status === true && item.price < priceRange
     );
   } else if (priceRange > 0) {
-    productsData = data.filter((item) => item.price < priceRange);
+    productsData = data?.filter((item: { price: number; }) => item.price < priceRange);
   } else {
     productsData = data;
   }
@@ -66,7 +72,7 @@ const dispatch = useAppDispatch()
         </div>
       </div>
       <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
-        {productsData?.map((product) => (
+        {productsData?.map((product: IProduct) => (
           <ProductCard product={product} />
         ))}
       </div>
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,1 +1,13 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: (name) => `/${name}`,
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = api;
