4761c6fefe28560fdd4394902573c97fa1c2cf90
Ismail Hossain <mdismail645221@gmail.com>
2023-09-22 09:57:14 +0600

    loader component added

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+    return (
+    
+        <div className="flex flex-col">
+          <div style={{border: "2px dashed red"}} className="w-12 h-12 rounded-full animate-spin  border-solid border-red-900 shadow-md"></div>
+        </div>
+      
+    );
+};
+
+export default Loader;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,10 +1,14 @@
+import Loader from '@/components/Loader';
 import ProductCard from '@/components/ProductCard';
 import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
 import { Switch } from '@/components/ui/switch';
 import { useToast } from '@/components/ui/use-toast';
 import { useGetProductsQuery } from '@/redux/api/apiSlice';
-import { priceStatus, priceToggle } from '@/redux/features/products/productSlice';
+import {
+  priceStatus,
+  priceToggle,
+} from '@/redux/features/products/productSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { IProduct } from '@/types/globalTypes';
 import { useEffect, useState } from 'react';
@@ -17,9 +21,8 @@ export default function Products() {
   //     .then((data) => setData(data));
   // }, []);
 
-   const {data, isLoading} = useGetProductsQuery('products')
-   console.log(data)
-
+  const { data, isLoading } = useGetProductsQuery('products');
+  console.log(data);
 
   // const {products, total} = useAppSelector(state=> state.cart)
 
@@ -27,32 +30,39 @@ export default function Products() {
 
   const { toast } = useToast();
 
-const {priceRange, status} = useAppSelector(state => state.product);
-const dispatch = useAppDispatch()
+  const { priceRange, status } = useAppSelector((state) => state.product);
+  const dispatch = useAppDispatch();
 
   const handleSlider = (value: number[]) => {
     console.log(value);
-    dispatch(priceToggle(value[0]))
+    dispatch(priceToggle(value[0]));
   };
 
   let productsData;
 
   if (status) {
     productsData = data?.filter(
-      (item: { status: boolean; price: number; }) => item.status === true && item.price < priceRange
+      (item: { status: boolean; price: number }) =>
+        item.status === true && item.price < priceRange
     );
   } else if (priceRange > 0) {
-    productsData = data?.filter((item: { price: number; }) => item.price < priceRange);
+    productsData = data?.filter(
+      (item: { price: number }) => item.price < priceRange
+    );
   } else {
     productsData = data;
   }
+
 
   return (
     <div className="grid grid-cols-12 max-w-7xl mx-auto relative ">
       <div className="col-span-3 z mr-10 space-y-5 border rounded-2xl border-gray-200/80 p-5 self-start sticky top-16 h-[calc(100vh-80px)]">
         <div>
           <h1 className="text-2xl uppercase">Availability</h1>
-          <div className="flex items-center space-x-2 mt-3" onClick={()=> dispatch(priceStatus())}>
+          <div
+            className="flex items-center space-x-2 mt-3"
+            onClick={() => dispatch(priceStatus())}
+          >
             <Switch id="in-stock" />
             <Label htmlFor="in-stock">In stock</Label>
           </div>
@@ -71,7 +81,9 @@ const dispatch = useAppDispatch()
           <div>From 0$ To {priceRange}$</div>
         </div>
       </div>
+
       <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
+        {!data && <Loader />}
         {productsData?.map((product: IProduct) => (
           <ProductCard product={product} />
         ))}
